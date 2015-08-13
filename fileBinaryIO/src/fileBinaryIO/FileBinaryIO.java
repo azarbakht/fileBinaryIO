c13d98fd631cf34272e92d5f582cf3e705ff1d6e
Emerson Amirhosein Azarbakht <azarbaam@eecs.oregonstate.edu>
2015-08-12 18:27:53 -0700

    FileInputStream and FileOutputStream

--- fileBinaryIO/src/fileBinaryIO/FileBinaryIO.java
+++ fileBinaryIO/src/fileBinaryIO/FileBinaryIO.java
@@ -1,5 +1,33 @@
 package fileBinaryIO;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class FileBinaryIO {
 
+	public static void main(String[] args) throws IOException{
+		
+		// OUTPUT ***************************************************************************
+		FileOutputStream output = new FileOutputStream("BinaryFile.dat");
+		
+		for (int i = 0; i <= 10; i++){
+			output.write(i);
+		}
+		
+		output.close();
+		
+		// INPUT ****************************************************************************
+		FileInputStream input = new FileInputStream("BinaryFile.dat");
+		
+		int value;
+		while((value = input.read()) != -1){
+			System.out.println(value + " \n");
+		}
+		
+		input.close();
+		
+		// OUTPUT with append ***************************************************************
+		
+	}
 }
